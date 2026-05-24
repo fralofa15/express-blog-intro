8a98ffe2fab7ef5d739c87f1ec29204a4e4f50aa
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-24 22:15:12 +0200

    aggiunto express creato rotte creato post.js

---
+++ main.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+    response.send("Server del mio blog");
+});
+
+app.get("/bacheca", (request, response) => {
+    response.json({
+        posts: posts
+    });
+});
+
+app.listen(1000, (error) => {
+    if (error) {
+        console.error("Il server ha riscontrato un errore");
+    } else {
+        console.log("Server in ascolto sulla porta 1000");
+    }
+});
